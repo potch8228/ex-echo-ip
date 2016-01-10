aa0aeca5fea57c83cc3eda5775ae6e98b59521a2
Tatsuya Kobayashi <pikopiko28@gmail.com>
2016-01-10 15:05:19 +0900

    Add Log output on X-Forwarded-For

--- lib/echo_ip/controllers/main.ex
+++ lib/echo_ip/controllers/main.ex
@@ -1,13 +1,17 @@
 defmodule EchoIp.Controllers.Main do
   use Sugar.Controller
+  require Logger
 
   def index(conn, []) do
     raw conn |> resp(200, "Hello world")
   end
 
   def ip(conn, []) do
-    {ip__, _} = conn.peer
-    ip_ = ip__
+    x_fwd = Plug.Conn.get_req_header(conn, "x-forwarded-for")
+    Logger.debug(x_fwd)
+    # {ip__, _} = conn.peer
+    # ip_ = ip__
+    ip_ = conn.remote_ip
           |> Tuple.to_list
           |> List.foldr "", fn (x, acc) -> "." <> Integer.to_string(x) <> acc end
     json conn, %{ip: String.slice(ip_, 1, String.length(ip_))}
